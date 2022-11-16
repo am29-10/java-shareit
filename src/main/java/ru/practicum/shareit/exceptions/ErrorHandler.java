48d4d0911be0c7046e134563182f9dd8407f4810
Artur <am29-10@mail.ru>
2022-11-16 17:21:32 +0300

    Добавить реализацию handler'а

--- src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpServerErrorException;
@@ -11,19 +12,21 @@ import java.io.IOException;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler({EntityNotFoundException.class})
+    @ExceptionHandler(EntityNotFoundException.class)
     public void handlerNotFound(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.NOT_FOUND.value());
     }
 
-    @ExceptionHandler({ValidationException.class})
+    @ExceptionHandler(ValidationException.class)
     public void handlerBadValidation(HttpServletResponse response) throws IOException {
         response.sendError(HttpStatus.CONFLICT.value());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public void handlerBadRequest(HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value());
+    public ResponseEntity<ErrorMessage> handlerBadRequest(IllegalArgumentException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorMessage(exception.getMessage()));
     }
 
     @ExceptionHandler(HttpServerErrorException.class)
