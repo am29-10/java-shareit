76f3f8017fe4e2ccca5d6f083bf870d3c3f4b262
Artur <am29-10@mail.ru>
2022-11-29 22:39:11 +0300

    Дополнить метод toItem функционалом для itemRequest

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,13 +1,16 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.booking.dto.BookingItemDto;
 import ru.practicum.shareit.item.comment.CommentDto;
 import ru.practicum.shareit.item.dto.ItemBookingDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
 
+@Component
 public class ItemMapper {
 
     public static Item toItem(ItemDto itemDto) {
@@ -17,6 +20,22 @@ public class ItemMapper {
                 .name(itemDto.getName())
                 .description(itemDto.getDescription())
                 .available(itemDto.getAvailable())
+                .owner(itemDto.getOwner())
+                .itemRequest(itemDto.getRequestId() == null ? null : ItemRequest.builder()
+                        .id(itemDto.getRequestId())
+                        .build())
+                .build();
+    }
+
+    public static ItemDto toItemDto(Item item) {
+        return ItemDto
+                .builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .available(item.getAvailable())
+                .owner(item.getOwner())
+                .requestId(item.getItemRequest() == null ? null : item.getItemRequest().getId())
                 .build();
     }
 
@@ -33,6 +52,4 @@ public class ItemMapper {
                 comments
         );
     }
-
-
 }
