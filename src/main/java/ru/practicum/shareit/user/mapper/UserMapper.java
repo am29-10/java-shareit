052c2e0a243c07f0252d5d7f196911c52340057a
Artur <am29-10@mail.ru>
2022-11-29 22:39:28 +0300

    Удалить неиспользуемый метод

--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -7,15 +7,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 @Component
 public class UserMapper {
 
-    public static UserDto toUserDto(User user) {
-        return UserDto
-                .builder()
-                .id(user.getId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .build();
-    }
-
     public static User toUser(UserDto userDto) {
         return User
                 .builder()
