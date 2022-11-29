249217c923b4c0b557c0a04ba8464eb0734ea9d2
Artur <am29-10@mail.ru>
2022-11-29 22:28:53 +0300

    Добавить импорт класса ItemRequest

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import javax.persistence.*;
@@ -32,4 +33,7 @@ public class Item {
     @ManyToOne
     @JoinColumn(name = "owner_id", referencedColumnName = "id")
     private User owner;
+    @ManyToOne
+    @JoinColumn(name = "item_request_id", referencedColumnName = "id")
+    private ItemRequest itemRequest;
 }
