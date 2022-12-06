6c3b49253fad4794f990b8df18080ff38fb6b1f7
Artur <am29-10@mail.ru>
2022-12-06 21:04:51 +0300

    Добавить класс в модуль Gateway

---
+++ server/src/main/java/ru/practicum/shareit/exceptions/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
