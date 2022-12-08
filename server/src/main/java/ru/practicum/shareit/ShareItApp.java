0183f0725019b5627f0ec782a9b8063b54772891
Artur <am29-10@mail.ru>
2022-12-08 19:21:05 +0300

    Удалить объявление порта

--- server/src/main/java/ru/practicum/shareit/ShareItApp.java
+++ server/src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShareItApp {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 9090);
 		SpringApplication.run(ShareItApp.class, args);
 	}
 
